9ba86b1fee86a311ec07974f4515a84b1a18ba6c
wangkangan <kyle.script@gmail.com>
2013-12-11 14:14:21 +0800

    新浪微博页面登陆  更新

--- KSharedSDK/KSinaWeiboLoginView.m
+++ KSharedSDK/KSinaWeiboLoginView.m
@@ -20,6 +20,7 @@
     UIWebView   *webView;
     UIButton    *dismissButton;
     
+    NSURL       *url;
     UIActivityIndicatorView *indicatorView;
 }
 
@@ -31,57 +32,54 @@
     return instance;
 }
 
-- (id)init
-{
-    if (self = [super init]) {
-        keyWindow = [UIApplication sharedApplication].keyWindow;
-        NSArray *windows = [UIApplication sharedApplication].windows;
-        if(windows.count > 0) {
-            keyWindow = [windows lastObject];
-        }
-        
-        //背景图片
-        CGRect screenBounds = [KUnits XYScreenBounds];
-        bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenBounds.size.width, screenBounds.size.height)];
-        bgView.backgroundColor = [UIColor blackColor];
-        bgView.opaque = YES;
-        bgView.alpha = 0.8f;
-        
-        //指示器
-        indicatorView = [[UIActivityIndicatorView alloc] initWithFrame : CGRectMake(0.0f, 0.0f, 88.0f, 88.0f)] ;
-        [indicatorView setCenter: CGPointMake(bgView.center.x, bgView.center.y - 50)];
-        [indicatorView setActivityIndicatorViewStyle: UIActivityIndicatorViewStyleWhiteLarge];
-        
-        //网页视图
-        webView = [[UIWebView alloc] initWithFrame:screenBounds];
-        webView.delegate = self;
-        webView.scalesPageToFit = NO;
-        webView.scrollView.scrollEnabled = NO;
-        
-        NSDictionary* params = [NSDictionary dictionaryWithObjectsAndKeys:
-                                kSinaWeiboAppKey,                @"client_id",       //申请的appkey
-                                kSinaWeiboRedirectURI,           @"redirect_uri",    //申请时的重定向地址
-                                @"mobile",                       @"display",         //web页面的显示方式
-                                @"all",                          @"scope",
-                                @"true",                         @"forcelogin",
-                                nil];
-        
-        NSURL *url = [KUnits generateURL:@"https://open.weibo.cn/oauth2/authorize" params:params];
-        NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-        [webView loadRequest:request];
-        
-        //退出按钮，暂时这样处理吧，以后这里用动画效果来处理一下
-        dismissButton = [[UIButton alloc] initWithFrame:CGRectMake(70, 280, 180, 58)];
-        [dismissButton setTitle:@"取消登陆" forState:UIControlStateNormal];
-        [dismissButton setBackgroundColor:[UIColor colorWithRed:222.0/255.0 green:70.0/255.0 blue:70.0/255.0 alpha:0.8]];
-        [dismissButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
-    }
-    return self;
-}
-
 - (void)setupViews
 {
+    keyWindow = [UIApplication sharedApplication].keyWindow;
+    NSArray *windows = [UIApplication sharedApplication].windows;
+    if(windows.count > 0) {
+        keyWindow = [windows lastObject];
+    }
+    
+    //背景图片
+    CGRect screenBounds = [KUnits XYScreenBounds];
+    bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenBounds.size.width, screenBounds.size.height)];
+    bgView.backgroundColor = [UIColor blackColor];
+    bgView.opaque = YES;
+    bgView.alpha = 0.8f;
+    
+    //指示器
+    indicatorView = [[UIActivityIndicatorView alloc] initWithFrame : CGRectMake(0.0f, 0.0f, 88.0f, 88.0f)] ;
+    [indicatorView setCenter: CGPointMake(bgView.center.x, bgView.center.y - 50)];
+    [indicatorView setActivityIndicatorViewStyle: UIActivityIndicatorViewStyleWhiteLarge];
+    
+    //网页视图
+    webView = [[UIWebView alloc] initWithFrame:screenBounds];
+    webView.delegate = self;
+    webView.scalesPageToFit = NO;
+    webView.scrollView.scrollEnabled = NO;
+    
+    NSDictionary* params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            kSinaWeiboAppKey,                @"client_id",       //申请的appkey
+                            kSinaWeiboRedirectURI,           @"redirect_uri",    //申请时的重定向地址
+                            @"mobile",                       @"display",         //web页面的显示方式
+                            @"all",                          @"scope",
+                            @"true",                         @"forcelogin",
+                            nil];
+    
+    url = [KUnits generateURL:@"https://open.weibo.cn/oauth2/authorize" params:params];
+    
+    //退出按钮，暂时这样处理吧，以后这里用动画效果来处理一下
+    dismissButton = [[UIButton alloc] initWithFrame:CGRectMake(70, 280, 180, 58)];
+    [dismissButton setTitle:@"取消登陆" forState:UIControlStateNormal];
+    [dismissButton setBackgroundColor:[UIColor colorWithRed:222.0/255.0 green:70.0/255.0 blue:70.0/255.0 alpha:0.8]];
+    [dismissButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    [webView loadRequest:request];
+    
     [indicatorView startAnimating];
+    [dismissButton setBackgroundColor:[UIColor colorWithRed:222.0/255.0 green:70.0/255.0 blue:70.0/255.0 alpha:0.6]];
+    
     [keyWindow addSubview:bgView];
     [keyWindow addSubview:indicatorView];
     [keyWindow addSubview:dismissButton];
